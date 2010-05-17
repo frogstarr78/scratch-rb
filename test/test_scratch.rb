9f34bc97f2593e76b34631f732f2662398f1c67a
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-05-16 23:34:34 -0700

    Hah I knew I was right about needing the compiling? method to be a comparison of array object_id's I'll have to fix the implementation of defining methods in scratch later... when I'm testing it.

--- test/test_scratch.rb
+++ test/test_scratch.rb
@@ -1,7 +1,55 @@
 require 'helper'
 
+class MockKernel
+  class << self
+    def puts *arguments
+      @puts = ['puts'] | arguments << "\n"
+    end
+
+    def print *arguments
+      @print = ['print'] | arguments
+    end
+
+    def puts?
+      @puts
+    end
+
+    def print?
+      @print
+    end
+  end
+end
+
 class TestScratch < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def terp 
+    @terp ||= Scratch::Scratch.new
+  end
+
+  context "stack" do
+    should "add literals to stack" do
+      terp.run '1 2 3'
+      assert_equal [1, 2, 3], terp.stack
+    end
+  end
+
+  context 'Scratch::PrintingWords' do
+    should "print element of from stack" do
+      terp.add_words( Scratch::PrintingWords )
+
+      terp.dictionary["PRINT"].expects(:call).with(terp)
+      terp.run '3'
+      assert_equal [3], terp.stack
+      terp.run "print"
+    end
+
+    should "raise StackTooSmall error when print called with an empty stack" do
+      terp.add_words( Scratch::PrintingWords )
+
+      assert_equal [], terp.stack
+      assert_raise Scratch::Scratch::StackTooSmall do
+#        terp.dictionary["PRINT"].call terp 
+        terp.run "print"
+      end
+    end
   end
 end
