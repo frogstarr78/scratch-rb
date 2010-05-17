e55944747cfffc9edbb580254a3d885302ffee90
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-05-16 23:35:08 -0700

    Moved more supporting code into outlying territories

---
+++ lib/scratch/variable.rb
@@ -0,0 +1,15 @@
+module Scratch
+  class Variable
+    def initialize v = 0
+      @value = v
+    end
+
+    def value
+      @value
+    end
+
+    def value= v = 0
+      @value = v
+    end
+  end
+end
