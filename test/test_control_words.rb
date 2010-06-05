9ffafee4989f7a624f3b2791c3c6d91dcf050f5b
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-06-05 12:46:24 -0700

    Fixed scaffold test

--- test/test_control_words.rb
+++ test/test_control_words.rb
@@ -3,7 +3,7 @@ require 'helper'
 class TestControlWords < TestHelper
   context 'Scratch::ControlWords' do
     %w(run ttimes is_true? is_false? if_else? continue? break? loop).each do |method|
-      should 'define :"' do
+      should "define :'#{method}'" do
         fail
         assert Scratch::ControlWords.instance_methods(false).include?( '"' )
       end
