fb320ce8e19a3c807d373573c2c8ff36564242b2
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-06-05 15:57:54 -0700

    Fixed tests they were named wrong

--- test/test_stack_words.rb
+++ test/test_stack_words.rb
@@ -1,77 +1,32 @@
 require 'helper'
 
-class TestMathWords < TestHelper
+class TestStackWords < TestHelper
 
-  context 'Scratch::VariableWords' do
-    %w(var store fetch).each do |meth|
+  context 'Scratch::StackWords' do
+    %w(dup drop swap over rot).each do |meth|
       should "define #{meth}"do
-        assert Scratch::VariableWords.instance_methods(false).include?( meth )
-      end
-    end
-
-  end
-
-  context "var method" do
-    should "initializes a variable" do
-      assert !terp.respond_to?( :a )
-      terp.run 'var a'
-      assert_equal [], terp.stack
-      assert_respond_to terp, :a
-      terp.a
-      assert_instance_of Scratch::Variable, terp.stack.pop
-    end
-
-    should "raise UnexpectedEOI error when called with an empty stack" do
-      assert_equal [], terp.stack
-      assert_raise Scratch::UnexpectedEOI do
-        terp.run "var"
+        assert Scratch::StackWords.instance_methods(false).include?( meth )
       end
     end
   end
 
-  context "store method" do
-    should "works" do
-      assert !terp.respond_to?( :b )
-      terp.run 'var b'
-      terp.run '1 b store'
-      assert_instance_of Scratch::Variable, terp.b.pop
-      assert_respond_to terp, :b
-    end
-
-    should "raise StackTooSmall error when called with an empty stack" do
-      terp.run 'var c'
-      assert_equal [], terp.stack
-      assert_raise Scratch::StackTooSmall do
-        terp.run "store"
-      end
-    end
-
-    should "raise StackTooSmall error when called with stack size of 1" do
-      terp.run 'var d'
-      assert_equal [], terp.stack
-      assert_raise Scratch::StackTooSmall do
-        terp.run "d store"
-      end
-    end
+  context "dup method" do
+    should "work"
   end
 
-  context "fetch method" do
-    should "works" do
-      assert !terp.respond_to?( :e )
-      terp.run 'var e'
-      terp.run '1 e store'
-      terp.run 'e fetch'
-      assert_equal [1], terp.stack
-    end
+  context "drop method" do
+    should "work"
+  end
 
-    should "raise StackTooSmall error when called with an empty stack" do
-      terp.run 'var f'
-      terp.run '1 f store'
-      assert_equal [], terp.stack
-      assert_respond_to terp, :f
-      assert_raise Scratch::StackTooSmall do
-        terp.run "fetch"
-      end
-    end
+  context "swap method" do
+    should "work"
+  end
+
+  context "over method" do
+    should "work"
+  end
+
+  context "rot method" do
+    should "work"
   end
 end
---
+++ test/test_variable_words.rb
@@ -0,0 +1,76 @@
+require 'helper'
+
+class TestVariableWords < TestHelper
+
+  context 'Scratch::VariableWords' do
+    %w(var store fetch).each do |meth|
+      should "define #{meth}"do
+        assert Scratch::VariableWords.instance_methods(false).include?( meth )
+      end
+    end
+  end
+
+  context "var method" do
+    should "initializes a variable" do
+      assert !terp.respond_to?( :a )
+      terp.run 'var a'
+      assert_equal [], terp.stack
+      assert_respond_to terp, :a
+      terp.a
+      assert_instance_of Scratch::Variable, terp.stack.pop
+    end
+
+    should "raise UnexpectedEOI error when called with an empty stack" do
+      assert_equal [], terp.stack
+      assert_raise Scratch::UnexpectedEOI do
+        terp.run "var"
+      end
+    end
+  end
+
+  context "store method" do
+    should "works" do
+      assert !terp.respond_to?( :b )
+      terp.run 'var b'
+      terp.run '1 b store'
+      assert_instance_of Scratch::Variable, terp.b.pop
+      assert_respond_to terp, :b
+    end
+
+    should "raise StackTooSmall error when called with an empty stack" do
+      terp.run 'var c'
+      assert_equal [], terp.stack
+      assert_raise Scratch::StackTooSmall do
+        terp.run "store"
+      end
+    end
+
+    should "raise StackTooSmall error when called with stack size of 1" do
+      terp.run 'var d'
+      assert_equal [], terp.stack
+      assert_raise Scratch::StackTooSmall do
+        terp.run "d store"
+      end
+    end
+  end
+
+  context "fetch method" do
+    should "works" do
+      assert !terp.respond_to?( :e )
+      terp.run 'var e'
+      terp.run '1 e store'
+      terp.run 'e fetch'
+      assert_equal [1], terp.stack
+    end
+
+    should "raise StackTooSmall error when called with an empty stack" do
+      terp.run 'var f'
+      terp.run '1 f store'
+      assert_equal [], terp.stack
+      assert_respond_to terp, :f
+      assert_raise Scratch::StackTooSmall do
+        terp.run "fetch"
+      end
+    end
+  end
+end
